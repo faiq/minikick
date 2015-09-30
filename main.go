026431a353b600ddd962d17083ba20b669ac1397
faiq <faiqrazarizvi@gmail.com>
2015-09-30 13:23:14 -0400

    use godeps

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/codegangsta/cli"
+	"github.com/faiq/minikick/Godeps/_workspace/src/github.com/codegangsta/cli"
 	"github.com/faiq/minikick/controllers"
 	"github.com/faiq/minikick/models"
 )
