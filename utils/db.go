282c3992f0ec0f1bc75036ba58fbb54995537902
faiq <faiqrazarizvi@gmail.com>
2015-10-01 02:33:06 -0400

    session will close whether i tell it to or not

--- utils/db.go
+++ utils/db.go
@@ -7,7 +7,6 @@ const uri = "mongodb://localhost/"
 // these commands dont happen concurrently, so this method will be fine
 func MakeDB(dbName string) *mgo.Database {
 	sess, err := mgo.Dial(uri)
-	defer sess.Close()
 	if err != nil {
 		panic(err) //might as well panic
 	}
